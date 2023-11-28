a96a96ca0e139e4eb329e1202563a998326eb41f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-28 11:07:05 +0300

    imported bootstrap, react-router, created a navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <navbar />
+      </BrowserRouter>
+   
 
     </div>
   );
